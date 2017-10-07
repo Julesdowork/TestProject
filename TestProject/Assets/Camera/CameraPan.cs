6c1bca227764aae87c786432e62727a9f30b3b34
Julian McNeill <julesdowork@gmail.com>
2017-10-07 19:35:02 -0400

    Reading from 2nd Stick

---
+++ TestProject/Assets/Camera/CameraPan.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour {
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = GameObject.FindWithTag("Player");
+	}
+
+	void LateUpdate ()
+	{
+//		float zPos = player.transform.position.z;
+//		transform.position = new Vector3(transform.position.x, transform.position.y, zPos);
+		transform.LookAt(player.transform);
+	}
+}
---
+++ TestProject/Assets/Camera/SelfieStick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfieStick : MonoBehaviour {
+
+	private GameObject player;
+	private Vector3 armRotation;
+
+	public float panSpeed = 10f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = GameObject.FindWithTag("Player");
+		armRotation = transform.rotation.eulerAngles;
+	}
+	
+	void Update ()
+	{
+		armRotation.y += Input.GetAxis("RHoriz") * panSpeed;
+		armRotation.x += Input.GetAxis("RVert") * panSpeed;
+		transform.position = player.transform.position;
+		transform.rotation = Quaternion.Euler(armRotation);
+	}
+}
