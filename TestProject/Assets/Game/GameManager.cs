2ea7e6bae369e67f0293938056ea3f7c75ab0e64
Julian McNeill <julesdowork@gmail.com>
2017-10-07 19:00:26 -0400

    Pausing Your Game

--- TestProject/Assets/Game/GameManager.cs
+++ TestProject/Assets/Game/GameManager.cs
@@ -5,13 +5,17 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class GameManager : MonoBehaviour {
 
+	private float initialFixedDelta;
+
 	public bool recording = true;
+	public bool paused = false;
 
 	void Start ()
 	{
 		PlayerPrefsManager.UnlockLevel(2);
 		Debug.Log(PlayerPrefsManager.IsLevelUnlocked(1));
 		Debug.Log(PlayerPrefsManager.IsLevelUnlocked(2));
+		initialFixedDelta = Time.fixedDeltaTime;
 	}
 	
 	// Update is called once per frame
@@ -21,5 +25,28 @@ public class GameManager : MonoBehaviour {
 			recording = false;
 		else
 			recording = true;
+
+		if (Input.GetKeyDown(KeyCode.P) && !paused)
+		{
+			PauseGame();
+		}
+		else if (Input.GetKeyDown(KeyCode.P) && paused)
+		{
+			ResumeGame();
+		}
+	}
+
+	void PauseGame ()
+	{
+		paused = true;
+		Time.timeScale = 0;
+		Time.fixedDeltaTime = 0;
+	}
+
+	void ResumeGame ()
+	{
+		paused = false;
+		Time.timeScale = 1f;
+		Time.fixedDeltaTime = initialFixedDelta;
 	}
 }
