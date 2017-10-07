fbc77a959f160b2c8b00e60b9616e22f9b3c176a
Julian McNeill <julesdowork@gmail.com>
2017-10-06 21:35:30 -0400

    Added a secret message script.

---
+++ TestProject/Assets/MyScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		print("It's close to midnight, and something evil's lurking in the dark");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
